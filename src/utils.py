01bf2f6eff54ad9386d4773f70b7548fb25080db
Nina Finageeva <finageeva.nina@mail.ru>
2024-02-20 18:00:36 +0300

    functional resolution - (utils.py)

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+from src.api_hh import HeadHunterAPI
+from src.api_sj import SuperJobAPI
+from src.jsonsaver import JSONSaver
+
+
+def user_hh():
+    pass
+
+
+def user_sj():
+    pass
+
+
+def user_choice_both():
+    pass
