13becd9982fe9ee3a92e956afe17cd4f33bfde14
Nina Finageeva <finageeva.nina@mail.ru>
2024-02-26 16:41:34 +0300

    information about vacation

--- src/vacancy.py
+++ src/vacancy.py
@@ -31,7 +31,7 @@ class Vacancy:
 
     def __lt__(self, other):
         if not isinstance(other, Vacancy):
-            raise TypeError()
+            raise TypeError('Вакансию можно сравнивать только с вакансией')
         return self.salary_from < other.salary_from
 
     def __gt__(self, other):
@@ -53,20 +53,53 @@ class Vacancy:
         """
         Возвращает вакансию в виде словаря
         """
-        pass
+        return {
+            'vacancy_title': self.vacancy_title,
+            'town': self.town,
+            'salary_from': self.salary_from,
+            'salary_to': self.salary_to,
+            'employment': self.employment,
+            'url': self.url
+        }
 
     @staticmethod
     def from_dict(vacancy_dict):
         """
         Возвращает вакансию в виде списка
         """
-        pass
+        return Vacancy(
+            vacancy_dict['vacancy_title'],
+            vacancy_dict['town'],
+            vacancy_dict['salary_from'],
+            vacancy_dict['salary_to'],
+            vacancy_dict['employment'],
+            vacancy_dict['url']
+        )
 
 
-    class Vacancies:
-        """
-        Обработка списка вакансий
-        """
+class Vacancies:
+    """
+    Обработка списка вакансий
+    """
+    def __init__(self):
+        self.__all_vacancies = []
 
-        def __init__(self):
-            self.__all_vacancies = []
+    def add_vacancies(self, new_vacancies):
+        self.__all_vacancies += new_vacancies
+
+    def delete_vacancies(self, old_vacancies):
+        for i in old_vacancies:
+            self.__all_vacancies.remove(i)
+
+    def sort_vacancies_by_salary(self):
+            self.__all_vacancies.sort(reverse=True)
+
+    @property
+    def all_vacancies(self):
+        return self.__all_vacancies
+
+    def to_list_dict(self):
+        a = []
+        for i in self.__all_vacancies:
+            a.append(i.to_dict())
+        return a
